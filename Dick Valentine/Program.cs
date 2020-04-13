6c259ca3211bc67a11259a1d4a499c806d38aa27
Anton Sidorov <CohoJET@gmail.com>
2020-04-13 23:34:48 +0300

    Initialize project

---
+++ Dick Valentine/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DickValentine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
